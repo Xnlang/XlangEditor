1377c4eb5bd639cfd261e48a69e9b7a271e59286
XueQing Chen <Xlang@vip.126.com>
2013-05-06 16:48:50 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,27 +1,16 @@
-<?php
-  $htmlData = '';
-	if (!empty($_POST['content1'])) {
-		if (get_magic_quotes_gpc()) {
-			$htmlData = stripslashes($_POST['content1']);
-		} else {
-			$htmlData = $_POST['content1'];
-		}
-	}
-?>
-<html>
-<head>
-  <link rel="stylesheet" href="https://Xnlang.cn/XlangEditor/themes/default/default.css" />
-  <link rel="stylesheet" href="https://Xnlang.cn/XlangEditor/plugins/code/prettify.css" />
-  <script charset="utf-8" src="https://xnlang.cn/XlangEditor/XlangEditor.js"></script>
-  <script charset="utf-8" src="https://Xnlang.cn/XlangEditor/lang/zh_CN.js"></script>
-  <script charset="utf-8" src="https://Xnlang.cn/XlangEditor/plugins/code/prettify.js"></script>
+ <link rel="stylesheet" href="http://Xnlang.cn/XlangEditor/themes/default/default.css" />
+  <link rel="stylesheet" href="http://Xnlang.cn/XlangEditor/plugins/code/prettify.css" />
+  <script charset="utf-8" src="http://xnlang.cn/XlangEditor/XlangEditor.js"></script>
+  <script charset="utf-8" src="http://Xnlang.cn/XlangEditor/lang/zh_CN.js"></script>
+  <script charset="utf-8" src="http://Xnlang.cn/XlangEditor/XlangEditor/plugins/code/prettify.js"></script>
   <script>
+  <head>
   <script type='text/javascript'>
 		XlangEditor.ready(function(X) {
 			var editor1 = X.create('textarea[name="content1"]', {
-				cssPath : 'https://Xnlang.cn/XlangEditor/themes/default/prettify.css',
-				uploadJson : 'https://Xnlang.cn/XlangEditor/php/upload_json.php',
-				fileManagerJson : 'https://Xnlang.cn/XlangEditor/php/file_manager_json.php',
+				cssPath : 'http://Xnlang.cn/XlangEditor/themes/default/prettify.css',
+				uploadJson : 'http://Xnlang.cn/XlangEditor/php/upload_json.php',
+				fileManagerJson : 'http://Xnlang.cn/XlangEditor/php/file_manager_json.php',
 				allowFileManager : true,
 				afterCreate : function() {
 					var self = this;
@@ -38,14 +27,11 @@
 			prettyPrint();
 		});
 	</script>
-</head>
-<body>
-	<?php echo $htmlData; ?>
-	<form name="example" method="post" action="demo.php">
-		<textarea name="content1" style="width:700px;height:200px;visibility:hidden;"><?php echo htmlspecialchars($htmlData); ?></textarea>
-		<br />
-		<input type="submit" name="button" value="提交内容" /> (提交快捷键: Ctrl + Enter)
-	</form>
+	</head>
+  <body>
+  <form name="example" method="post" action="demo.php">
+    <textarea name="content1" style="width:700px;height:200px;visibility:hidden;"></textarea>
+    <br />
+    <input type="submit" name="button" value="提交内容" /> (提交快捷键: Ctrl + Enter)
+  </form>
 </body>
-</html>
-
